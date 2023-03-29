1c86ef8a3a82c5f71fcc89efdc29fe75cb94cd53
Annas Dwi <manasdwi276@gmail.com>
2023-03-29 10:19:43 +0700

    add colors and font style

--- src/Theme.styled.js
+++ src/Theme.styled.js
@@ -1,3 +1,34 @@
 export const theme = {
-  colors: {},
+  colors: {
+    primary: {
+      blue: "#2F80ED",
+      darkGray: "#4F4F4F",
+      lightGray: "#828282",
+      softGray: "#E0E0E0",
+      white: "#F2F2F2",
+    },
+    indicator: {
+      orange: "#F8B76B",
+      purple: "#8785FF",
+      red: "#EB5757",
+      yellow: "#F2C94C",
+    },
+    chats: {
+      baseYellow: "#FCEED3",
+      outlineYellow: "#E5A443",
+      basePurple: "#EEDCFF",
+      outlinePurple: "#9B51E0",
+      baseGreen: "#D2F2EA",
+      outlineGreen: "#43B78D",
+    },
+    stickers: {
+      softBlue: "#E9F3FF",
+      softChoco: "#FDCFA4",
+      softLightChoco: "#F9E9C3",
+      softGreen: "#AFEBDB",
+      softLightGreen: "#CBF1C2",
+      softPurple: "#CFCEF9",
+      softLightPurple: "#F9E0FD",
+    },
+  },
 };
